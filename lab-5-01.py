1f36ad0128338bb48a3333039dd21bd8ca5f0ea2
Maxim Borysuik <113389561+BoriSSyk@users.noreply.github.com>
2022-09-26 08:43:56 +0300

    Update lab-5-01.py

--- lab-5-01.py
+++ lab-5-01.py
@@ -3,9 +3,9 @@ x = int(input("Введіть x: "))
 if x >= 5.1:
     y = math.log2(3*x)-7*math.sqrt(x)
     print(y)
-elif -0.7 < x < 5.1:
-    y = math.exp(x)+2*math.pow(x, 3)
+elif -0.7 < x:
+    y = math.pow(e, x)+2*math.pow(x, 3)
     print(y)
 elif x <= -0.7:
-    y = math.exp(x)+math.sin(x+math.pi/4)
+    y = math.pow(e, x)+math.sin(x+math.pi/4)
     print(y)
